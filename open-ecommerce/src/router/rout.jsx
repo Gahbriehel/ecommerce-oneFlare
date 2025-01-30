fe4cfc90441439f0f634e96fbc5f46b45a722e3d
gahbriehel <babatise002@gmail.com>
2025-01-30 20:47:21 +0100

    feat: added cart functionality and updated routing for product details

--- open-ecommerce/src/router/rout.jsx
+++ open-ecommerce/src/router/rout.jsx
@@ -4,15 +4,18 @@ import Home from '../components/Home'
 import ProductDetails from '../components/ProductDetails'
 import Contact from '../components/Contact'
 import Checkout from '../components/Checkout'
+import CartPage from '../components/CartPage'
+// import updateQuantity from '../components/updateQuantity'
 
 
-const Rout = ({ addToCart }) => {
+const Rout = ({ addToCart, removeFromCart, cartItems, updateQuantity }) => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home addToCart={addToCart} />} />
         <Route path='/checkout' element={<Checkout />} />
-        <Route path='/product/:id' element={<ProductDetails addToCart={addToCart} />} />
+        <Route path='/product/:id' element={<ProductDetails addToCart={addToCart} cartItems={cartItems} updateQuantity={updateQuantity} />} />
+        <Route path='/cart' element={<CartPage cartItems={cartItems} removeFromCart={removeFromCart} updateQuantity={updateQuantity} />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
     </>
