a089576615d5034bf4b0d05fb4e08f172857a4f2
gahbriehel <babatise002@gmail.com>
2025-01-31 11:43:46 +0100

    feat: enhanced CartPage with checkout functionality and styling

--- open-ecommerce/src/components/CartPage.jsx
+++ open-ecommerce/src/components/CartPage.jsx
@@ -1,4 +1,5 @@
 import './css/cartPage.css'
+import Footer from "./Country";
 import { TbShoppingBag } from "react-icons/tb";
 import { FaRegTrashAlt } from "react-icons/fa";
 import QuantityCounter from './QuantityCounter';
@@ -28,49 +29,72 @@ const CartPage = ({ cartItems, updateQuantity, removeFromCart }) => {
                         </button>
                     </div>
                 ) : (
-                    <div className="cart-content">
-                        <table>
-                            <thead>
-                                <tr className='cart-top'>
-                                    <th className='first-child'>product</th>
-                                    <th>price</th>
-                                    <th>quantity</th>
-                                    <th>subtotal</th>
-                                </tr>
-                            </thead>
-                            {
-                                cartItems.map((item) => {
-                                    return (
-                                        <>
-                                            <tbody key={item.id}>
-                                                <tr className='table-details'>
-                                                    <td className='cart-item-image'>
-                                                        <img src={item.image} alt="item image" />
-                                                        <span> {item.name}</span>
-                                                    </td>
-                                                    <td>₦{new Intl.NumberFormat('en-us').format(item.price)}</td>
-                                                    <td>
-                                                        <QuantityCounter
-                                                            className="item-quantityy"
-                                                            quantity={item.quantity}
-                                                            onUpdateQuantity={(newQuantity) => {
-                                                                updateQuantity(item.id, newQuantity)
-                                                            }}
-                                                        />
-                                                    </td>
-                                                    <td>₦{new Intl.NumberFormat('en-us').format(calculateTotal())}</td>
-                                                    <td><button onClick={() => removeFromCart(item.id)}><FaRegTrashAlt /></button></td>
-                                                </tr>
-                                            </tbody>
-                                        </>
-                                    )
-                                })
-                            }
-                        </table>
+                    <div className="cart-content0">
+                        <div className="cart-content">
+                            <table>
+                                <thead>
+                                    <tr className='cart-top'>
+                                        <th className='first-child'>product</th>
+                                        <th>price</th>
+                                        <th>quantity</th>
+                                        <th>subtotal</th>
+                                    </tr>
+                                </thead>
+                                {
+                                    cartItems.map((item) => {
+                                        return (
+                                            <>
+                                                <tbody key={item.id}>
+                                                    <tr className='table-details'>
+                                                        <td className='cart-item-image'>
+                                                            <img src={item.image} alt="item image" />
+                                                            <span> {item.name}</span>
+                                                        </td>
+                                                        <td>₦{new Intl.NumberFormat('en-us').format(item.price)}</td>
+                                                        <td>
+                                                            <QuantityCounter
+                                                                className="item-quantityy"
+                                                                quantity={item.quantity}
+                                                                onUpdateQuantity={(newQuantity) => {
+                                                                    updateQuantity(item.id, newQuantity)
+                                                                }}
+                                                            />
+                                                        </td>
+                                                        <td>₦{new Intl.NumberFormat('en-us').format(item.price * item.quantity)}</td>
+                                                        <td><button onClick={() => removeFromCart(item.id)}><FaRegTrashAlt /></button></td>
+                                                    </tr>
+                                                </tbody>
+                                            </>
+                                        )
+                                    })
+                                }
+                            </table>
+                        </div>
+                        <div className="cartPage-checkout-container">
+                            <div className="checkout-coupon">
+                                <input type="text" placeholder='Coupon code' />
+                                <button>Apply Coupon</button>
+                            </div>
+                            <div className="cartPage-checkout">
+                                <h3>Cart Total</h3>
+                                <hr />
+                                <div className="total-amount">
+                                    <p>Total:</p>
+                                    <p>₦{new Intl.NumberFormat('en-us').format(calculateTotal())}</p>
+                                </div>
+                                <hr />
+                                <p>Taxes and shipping will be calculated at checkout</p>
+                                <hr />
+                                <button>Proceed to Checkout</button>
+                            </div>
+                        </div>
+                        <Footer />
                     </div>
+
                 )
                 }
             </div>
+
 
         </div >
     )
