cb08f50799d0e6b0ac9cb96d139bd31f5215dd8f
gahbriehel <babatise002@gmail.com>
2025-02-04 16:49:47 +0100

    feat: added confirmation route and updated product details route to include toggleCart prop

--- open-ecommerce/src/router/rout.jsx
+++ open-ecommerce/src/router/rout.jsx
@@ -5,16 +5,18 @@ import ProductDetails from '../components/ProductDetails'
 import Contact from '../components/Contact'
 import Checkout from '../components/Checkout'
 import CartPage from '../components/CartPage'
+import Confirmation from '../components/Confirmation'
 // import updateQuantity from '../components/updateQuantity'
 
 
-const Rout = ({ addToCart, removeFromCart, cartItems, updateQuantity }) => {
+const Rout = ({ addToCart, removeFromCart, cartItems, updateQuantity, toggleCart }) => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home addToCart={addToCart} />} />
         <Route path='/checkout' element={<Checkout cartItems={cartItems} />} />
-        <Route path='/product/:id' element={<ProductDetails addToCart={addToCart} cartItems={cartItems} updateQuantity={updateQuantity} />} />
+        <Route path='/checkout/confirmation' element={<Confirmation />} />
+        <Route path='/product/:id' element={<ProductDetails addToCart={addToCart} cartItems={cartItems} updateQuantity={updateQuantity} toggleCart={toggleCart} />} />
         <Route path='/cart' element={<CartPage cartItems={cartItems} removeFromCart={removeFromCart} updateQuantity={updateQuantity} />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
