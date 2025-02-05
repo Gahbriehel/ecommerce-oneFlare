1f6b2bfd5ea623454edf488d874cd9f461fdbc2c
gahbriehel <babatise002@gmail.com>
2025-02-05 13:35:42 +0100

    feat: added filters-toggler styles for improved filter visibility on small screens

--- open-ecommerce/src/components/Filters.jsx
+++ open-ecommerce/src/components/Filters.jsx
@@ -1,17 +1,21 @@
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { useState } from "react";
 import homeProducts from "./Homeproducts";
+import { GiSettingsKnobs } from "react-icons/gi";
 import './css/filter.css'
 const Filters = () => {
     const [products, setProducts] = useState(homeProducts)
     console.log(setProducts)
 
-
+    const [showFilters, setShowFilters] = useState(false)
     const [openAvailability, setOpenAvailability] = useState(false)
     const [openPriceFilter, setOpenPriceFilter] = useState(false)
     const [openCategoryFilter, showOpenCategoryFilter] = useState(false)
     const [openAlphabetFilter, setOpenAlphabetFilter] = useState(false)
 
+    const handleShowFilters = (e) => {
+        e.preventDefault(setShowFilters(!showFilters))
+    }
 
     const handleShowAvailability = (e) => {
         e.preventDefault(setOpenAvailability(!openAvailability))
@@ -38,96 +42,100 @@ const Filters = () => {
                         <div className="filter-container-text">
                             <p>Showing products 1 - 20 of {products.length} products</p>
                         </div>
-                        <div className="filter-section">
-                            <div className="filter-section-text">
-                                <p><strong>Filter:</strong></p>
-                            </div>
-                            <div className="filter-input">
-                                <button onClick={handleShowAvailability} className="availability-btn">
-                                    <span>Availability</span>
-                                    <MdKeyboardArrowDown className="arrow-icon" />
-                                </button>
-                                {openAvailability && (
-                                    <div className="availability-container">
-                                        <div className="availability-input">
-                                            <input type="checkbox" id="in-stock" />
-                                            <label htmlFor="in-stock"> In stock (😉)</label>
-                                        </div>
-                                        <div className="availability-input">
-                                            <input type="checkbox" id="out-stock" />
-                                            <label htmlFor="out-stock"> Out of stock (😉)</label>
-                                        </div>
+                        <button onClick={handleShowFilters} className="filters-toggler"><GiSettingsKnobs /></button>
+                        {
+                            showFilters && (
+                                <div className="filter-section">
+                                    <div className="filter-section-text">
+                                        <p><strong>Filter:</strong></p>
                                     </div>
-                                )
-                                }
-                            </div>
-                            <div className="filter-input">
-                                <button onClick={handleShowPriceFilter} className="range-btn">
-                                    <span>Price</span>
-                                    <span><MdKeyboardArrowDown className="arrow-icon2" /></span>
-                                </button>
-                                {
-                                    openPriceFilter && (
-                                        <div className="price-range-container">
-                                            <div className="range-input">
-                                                <label htmlFor="from">From:</label>
-                                                <br />
-                                                <input type="text" inputMode="numeric" placeholder="Enter amount" id="from" />
+                                    <div className="filter-input">
+                                        <button onClick={handleShowAvailability} className="availability-btn">
+                                            <span>Availability</span>
+                                            <MdKeyboardArrowDown className="arrow-icon" />
+                                        </button>
+                                        {openAvailability && (
+                                            <div className="availability-container">
+                                                <div className="availability-input">
+                                                    <input type="checkbox" id="in-stock" />
+                                                    <label htmlFor="in-stock"> In stock (😉)</label>
+                                                </div>
+                                                <div className="availability-input">
+                                                    <input type="checkbox" id="out-stock" />
+                                                    <label htmlFor="out-stock"> Out of stock (😉)</label>
+                                                </div>
                                             </div>
-                                            <div className="range-input">
-                                                <label htmlFor="to">To:</label>
-                                                <br />
-                                                <input type="text" inputMode="numeric" placeholder="Enter amount" id="to" />
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                            </div>
-                            <div className="filter-input">
-                                <button onClick={handleShowCategoryFilter} className="category-btn">
-                                    <span>Category</span>
-                                    <span><MdKeyboardArrowDown /></span>
-                                </button>
-                                {
-                                    openCategoryFilter && (
-                                        <div className="category-container">
-                                            <div className="category-input">
-                                                <input type="checkbox" id="bags" />
-                                                <label htmlFor="bags"> Bags</label>
-                                            </div>
-                                            <div className="category-input">
-                                                <input type="checkbox" id="shoes" />
-                                                <label htmlFor="shoes"> Shoes</label>
-                                            </div>
-                                            <div className="category-input">
-                                                <input type="checkbox" id="clothes" />
-                                                <label htmlFor="clothes"> Clothes</label>
-                                            </div>
-                                            <div className="category-input">
-                                                <input type="checkbox" id="accessories" />
-                                                <label htmlFor="accessories"> Accessories</label>
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                            </div>
-                            <div className="filter-input">
-                                <button onClick={handleShowAlphabetFilter} className="alpha-category-btn">Alphabetically A - Z<span><MdKeyboardArrowDown /></span></button>
-                                {
-                                    openAlphabetFilter && (
-                                        <div className="alphabet-container">
-                                            <div className="alpha-category-input">
-                                                <span>Alphabetically from A-Z</span>
-                                            </div>
-                                            <div className="alpha-category-input">
-                                                <span>Alphabetically from Z-A</span>
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                            </div>
-                        </div>
-
+                                        )
+                                        }
+                                    </div>
+                                    <div className="filter-input">
+                                        <button onClick={handleShowPriceFilter} className="range-btn">
+                                            <span>Price</span>
+                                            <span><MdKeyboardArrowDown className="arrow-icon2" /></span>
+                                        </button>
+                                        {
+                                            openPriceFilter && (
+                                                <div className="price-range-container">
+                                                    <div className="range-input">
+                                                        <label htmlFor="from">From:</label>
+                                                        <br />
+                                                        <input type="text" inputMode="numeric" placeholder="Enter amount" id="from" />
+                                                    </div>
+                                                    <div className="range-input">
+                                                        <label htmlFor="to">To:</label>
+                                                        <br />
+                                                        <input type="text" inputMode="numeric" placeholder="Enter amount" id="to" />
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                    <div className="filter-input">
+                                        <button onClick={handleShowCategoryFilter} className="category-btn">
+                                            <span>Category</span>
+                                            <span><MdKeyboardArrowDown /></span>
+                                        </button>
+                                        {
+                                            openCategoryFilter && (
+                                                <div className="category-container">
+                                                    <div className="category-input">
+                                                        <input type="checkbox" id="bags" />
+                                                        <label htmlFor="bags"> Bags</label>
+                                                    </div>
+                                                    <div className="category-input">
+                                                        <input type="checkbox" id="shoes" />
+                                                        <label htmlFor="shoes"> Shoes</label>
+                                                    </div>
+                                                    <div className="category-input">
+                                                        <input type="checkbox" id="clothes" />
+                                                        <label htmlFor="clothes"> Clothes</label>
+                                                    </div>
+                                                    <div className="category-input">
+                                                        <input type="checkbox" id="accessories" />
+                                                        <label htmlFor="accessories"> Accessories</label>
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                    <div className="filter-input">
+                                        <button onClick={handleShowAlphabetFilter} className="alpha-category-btn">Alphabetically A - Z<span><MdKeyboardArrowDown /></span></button>
+                                        {
+                                            openAlphabetFilter && (
+                                                <div className="alphabet-container">
+                                                    <div className="alpha-category-input">
+                                                        <span>Alphabetically from A-Z</span>
+                                                    </div>
+                                                    <div className="alpha-category-input">
+                                                        <span>Alphabetically from Z-A</span>
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                </div>
+                            )
+                        }
                     </div>
                 </form>
             </div>
