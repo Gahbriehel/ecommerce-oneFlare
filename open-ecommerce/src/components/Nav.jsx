7d021b341ed6423aa11fd12aaa8e2e9c5040e01a
gahbriehel <babatise002@gmail.com>
2025-02-05 13:01:05 +0100

    feat: redesigned Nav component for improved layout and added responsive search functionality

--- open-ecommerce/src/components/Nav.jsx
+++ open-ecommerce/src/components/Nav.jsx
@@ -9,39 +9,47 @@ const Nav = ({ toggleCart, cartItemsCount }) => {
     return (
         <>
             <div className="nav-container">
-                <div className="nav-left">
-                    <ul>
-                        <li>
-                            <Link to="/" className="nav-link logo">
-                                <img src="/images/logo.png" alt="Logo" />
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/" className="nav-link business-name">
-                                <strong>Slack Market and Shops</strong>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                to="/"
-                                className={currentPage === '/' ? 'nav-link link-active' : 'nav-link'}
-                            >
-                                Shop
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="nav-right">
-                    <div className="search">
-                        <div className="search-container">
-                            <input type="text" className="search-input" placeholder="Search" />
-                            <span><IoIosSearch className="search-icon" /></span>
-                        </div>
+                <div className="big-nav">
+                    <div className="nav-left">
+                        <ul>
+                            <li>
+                                <Link to="/" className="nav-link logo">
+                                    <img src="/images/logo.png" alt="Logo" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/" className="nav-link business-name">
+                                    <strong>Slack Market and Shops</strong>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/"
+                                    className={currentPage === '/' ? 'nav-link link-active shop-text' : 'nav-link shop-text'}
+                                >
+                                    Shop
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
-                    <span onClick={toggleCart} className="cart-icon">
-                        <p><TbShoppingBag /></p>
-                        {cartItemsCount > 0 && <sup className="cart-count">{cartItemsCount}</sup>}
-                    </span>
+                    <div className="nav-right">
+                        <div className="search">
+                            <div className="search-container">
+                                <input type="text" className="search-input" placeholder="Search" />
+                                <span><IoIosSearch className="search-icon" /></span>
+                            </div>
+                        </div>
+                        <span onClick={toggleCart} className="cart-icon">
+                            <p><TbShoppingBag /></p>
+                            {cartItemsCount > 0 && <sup className="cart-count">{cartItemsCount}</sup>}
+                        </span>
+                    </div>
+                </div>
+                <div className="small-search">
+                    <div className="search-container1">
+                        <input type="text" className="search-input1" placeholder="Search" />
+                        <span><IoIosSearch className="search-icon1" /></span>
+                    </div>
                 </div>
             </div>
             <hr />
