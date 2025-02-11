d1638e508e50247bb2a883edbd67f324ee71c15b
gahbriehel <babatise002@gmail.com>
2025-02-11 12:47:47 +0100

    feat: enhanced product display with Radix UI components and improved layout

--- open-ecommerce/src/components/Home.jsx
+++ open-ecommerce/src/components/Home.jsx
@@ -5,11 +5,12 @@ import homeProducts from "./Homeproducts";
 import { Link, useLocation } from "react-router-dom";
 import Filters from './Filters';
 import { TbShoppingBag } from "react-icons/tb";
+import { IconButton, Tooltip } from '@radix-ui/themes';
+import { PiPlusCircleDuotone } from 'react-icons/pi';
 
 const Home = ({ addToCart }) => {
     const [products, setProducts] = useState(homeProducts)
     console.log(setProducts)
-
 
     return (
         <div className="home-container">
@@ -27,13 +28,19 @@ const Home = ({ addToCart }) => {
                                                 <Link to={`/product/${product.id}`} state={{ product, products }}><img src={product.image} alt="product" className="product-image"></img></Link>
                                             </div>
                                             <div className="product-info">
-                                                <Link className='category-link'><p className='product-category-p'>{product.category}</p></Link>
-                                                <p className='product-name-p'>{product.name}</p>
-                                                <p className="old-price">₦{new Intl.NumberFormat('en-us', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(product.oldPrice)}</p>
-                                                <p className="price">₦{new Intl.NumberFormat('en-us', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(product.price)}</p>
-                                                <button onClick={() => addToCart(product)}><TbShoppingBag />
-                                                    <span className="add-to-cart-text">Add to Cart</span>
-                                                </button>
+                                                <div className="product-info-top">
+                                                    <Link className='category-link'><p className='product-category-p'>{product.category}</p></Link>
+                                                    <p className='product-name-p'>{product.name}</p>
+                                                    <p className="old-price">₦{new Intl.NumberFormat('en-us', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(product.oldPrice)}</p>
+                                                    <p className="price">₦{new Intl.NumberFormat('en-us', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(product.price)}</p>
+                                                </div>
+                                                <div>
+                                                    <Tooltip content="Add to cart">
+                                                        <IconButton radius="full">
+                                                            <TbShoppingBag className='shoppingBag' />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
