da39070ea2363d305cc6c0ac3c374cb142c73872
gahbriehel <babatise002@gmail.com>
2025-02-11 12:48:02 +0100

    feat: wrapped App component with Radix UI Theme for improved theming support

--- open-ecommerce/src/main.jsx
+++ open-ecommerce/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './components/css/index.css'
+import { Theme } from '@radix-ui/themes'
 import './components/css/fonts.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </StrictMode>,
 )
